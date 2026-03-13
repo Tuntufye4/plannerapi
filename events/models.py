e067f1cc810418504718d3dcaafda342ee4ee732
Tuntufye4 <tuntumwanyongo@gmail.com>
2026-03-13 14:37:30 +0200

    renamed events field

--- events/models.py
+++ events/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class EventItem(models.Model):
     event_name = models.CharField(max_length=200)
     item_name = models.CharField(max_length=200)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    paid = models.BooleanField(default=False) 
+    cost = models.DecimalField(max_digits=10, decimal_places=2)                 
+    completed = models.BooleanField(default=False) 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):    
