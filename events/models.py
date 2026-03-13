d14447ad657af9c2a9434e06949c633805448ac0
Tuntufye4 <tuntumwanyongo@gmail.com>
2026-03-13 14:28:37 +0200

    added paid

--- events/models.py
+++ events/models.py
@@ -4,7 +4,8 @@ class EventItem(models.Model):
     event_name = models.CharField(max_length=200)
     item_name = models.CharField(max_length=200)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+    paid = models.BooleanField(default=False) 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.event_name} - {self.item_name}"            
+    def __str__(self):    
+        return f"{self.event_name} - {self.item_name}"                 
